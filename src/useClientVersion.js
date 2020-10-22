d8d09bb999de27a99b491ce59755654234646696
yqrashawn <namy.19@gmail.com>
2020-10-22 21:23:40 +0800

    fix(useClientVersion): use provider.call when using js sdk

--- src/useClientVersion.js
+++ src/useClientVersion.js
@@ -6,12 +6,13 @@ const GET_CLIENT_VERSION_SWR_ID = "GET_CLIENT_VERSION_SWR_ID";
 export default function useClientVersion() {
   const confluxJSDefined = useConfluxJSDefined();
 
-  const {
-    data: clientVersion,
-    error: getClientVersionError,
-    mutate,
-  } = useSWR(GET_CLIENT_VERSION_SWR_ID, () =>
-    window?.confluxJS?.provider?.send("cfx_clientVersion", [])
+  const { data: clientVersion, error: getClientVersionError, mutate } = useSWR(
+    confluxJSDefined ? GET_CLIENT_VERSION_SWR_ID : null,
+    () => {
+      if (typeof window?.confluxJS?.provider?.send === "function")
+        return window?.confluxJS?.provider?.send("cfx_clientVersion", []);
+      return window?.confluxJS?.provider?.call("cfx_clientVersion");
+    }
   );
 
   useEffect(() => {
