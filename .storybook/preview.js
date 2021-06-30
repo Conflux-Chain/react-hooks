c793133f95b743821dde2df2c7a9a1f5e2b0c279
ConfluxBot <confluxbot@users.noreply.github.com>
2021-06-30 07:10:30 +0000

    feat: release to gh-pages [ci skip]

--- .storybook/preview.js
+++
@@ -1,5 +0,0 @@
-import "@storybook/addon-console";
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-};
--- src/utils.js
+++
@@ -1,8 +0,0 @@
-import Big from "big.js";
-
-export function to18Decimal(num, originalDecimal) {
-  num = Big(num);
-  originalDecimal = Number(originalDecimal);
-
-  return num.times(1e18).div(Big(10).pow(originalDecimal));
-}
