c2071fcef5c26441050612c7b92d77ee6b595c20
yqrashawn <namy.19@gmail.com>
2021-12-17 17:19:19 +0800

    fix: use window.conflux, remove window.confluxJS.provider

--- src/useClientVersion.js
+++ src/useClientVersion.js
@@ -1,26 +1,27 @@
-import { useSWR, useConfluxJSDefined } from "./";
-import { useEffect } from "react";
+import { useSWR, useConfluxJSDefined } from "./"
+import { useEffect } from "react"
 
-const GET_CLIENT_VERSION_SWR_ID = "GET_CLIENT_VERSION_SWR_ID";
+const GET_CLIENT_VERSION_SWR_ID = "GET_CLIENT_VERSION_SWR_ID"
 
 export default function useClientVersion() {
-  const confluxJSDefined = useConfluxJSDefined();
+  const confluxJSDefined = useConfluxJSDefined()
 
-  const { data: clientVersion, error: getClientVersionError, mutate } = useSWR(
-    confluxJSDefined ? GET_CLIENT_VERSION_SWR_ID : null,
-    () => {
-      if (typeof window?.confluxJS?.provider?.send === "function")
-        return window?.confluxJS?.provider?.send("cfx_clientVersion", []);
-      return window?.confluxJS?.provider?.call("cfx_clientVersion");
-    }
-  );
+  const {
+    data: clientVersion,
+    error: getClientVersionError,
+    mutate,
+  } = useSWR(confluxJSDefined ? GET_CLIENT_VERSION_SWR_ID : null, () => {
+    if (typeof window?.conflux?.send === "function")
+      return window?.conflux?.send("cfx_clientVersion", [])
+    return window?.conflux?.call?.("cfx_clientVersion")
+  })
 
   useEffect(() => {
-    if (confluxJSDefined) mutate();
-  }, [confluxJSDefined]);
+    if (confluxJSDefined) mutate()
+  }, [confluxJSDefined])
 
   if (getClientVersionError)
-    console.error(`Error get clientVersion: ${getClientVersionError?.message}`);
+    console.error(`Error get clientVersion: ${getClientVersionError?.message}`)
 
-  return clientVersion;
+  return clientVersion
 }
